bd09af184976c34eec80d50fa398d01ad7a54f2f
Thomas Kaegi <tk@meeloo.ch>
2016-03-05 11:16:22 +0100

    Fixes #2

--- help.php
+++ help.php
@@ -1,55 +1,79 @@
-<p><?=$this->i18n('help_intro');?></p>
-
-<p><b><?=$this->i18n('config_street');?></b></p>
 <?php
+	echo rex_view::info($this->i18n('help_intro'));
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getStreet();".PHP_EOL;
 	$code .= "?>";
-?>
-<pre><?=highlight_string($code,true);?></pre>
-
-<p><b><?=$this->i18n('config_zipcode');?></b></p>
-<?php
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_street'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getZipcode();".PHP_EOL;
 	$code .= "?>";
-?>
-<pre><?=highlight_string($code,true);?></pre>
-
-<p><b><?=$this->i18n('config_city');?></b></p>
-<?php
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_zipcode'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getCity();".PHP_EOL;
 	$code .= "?>";
-?>
-<pre><?=highlight_string($code,true);?></pre>
-
-<p><b><?=$this->i18n('config_country');?></b></p>
-<?php
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_city'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getCountry();".PHP_EOL;
 	$code .= "?>";
-?>
-<pre><?=highlight_string($code,true);?></pre>
-
-<p><b><?=$this->i18n('config_latitude');?></b></p>
-<?php
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_country'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getLatitude();".PHP_EOL;
 	$code .= "?>";
-?>
-<pre><?=highlight_string($code,true);?></pre>
-
-<p><b><?=$this->i18n('config_longitude');?></b></p>
-<?php
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_latitude'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getLongitude();".PHP_EOL;
 	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_longitude'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
 ?>
-<pre><?=highlight_string($code,true);?></pre>
