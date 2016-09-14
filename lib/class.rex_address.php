3867f3a79fda83a29e150ffc17a461ae7affb3d5
Thomas Kaegi <tk@meeloo.ch>
2016-09-14 11:33:10 +0200

    Methoden für neue Felder hinzugefügt

--- lib/class.rex_address.php
+++ lib/class.rex_address.php
@@ -1,5 +1,21 @@
 <?php
 	class rex_address {
+		public static function getTelephone() {
+			return rex_addon::get('rex_address')->getConfig('telephone');
+		}
+		
+		public static function getFax() {
+			return rex_addon::get('rex_address')->getConfig('fax');
+		}
+		
+		public static function getEmail() {
+			return rex_addon::get('rex_address')->getConfig('email');
+		}
+		
+		public static function getInfo() {
+			return rex_addon::get('rex_address')->getConfig('info');
+		}
+		
 		public static function getStreet() {
 			return rex_addon::get('rex_address')->getConfig('street');
 		}
