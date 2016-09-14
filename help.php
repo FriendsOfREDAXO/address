b4b5336b13bf3593ae6772ac52adbb622640eac5
Thomas Kaegi <tk@meeloo.ch>
2016-09-14 11:31:17 +0200

    Neue Methoden ergänzt

--- help.php
+++ help.php
@@ -3,6 +3,58 @@
 	
 	$code = "";
 	$code .= "<?php".PHP_EOL;
+	$code .= "	echo rex_address::getTelephone();".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_telephone'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
+	$code .= "	echo rex_address::getFax();".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_fax'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
+	$code .= "	echo rex_address::getEmail();".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_email'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
+	$code .= "	echo rex_address::getInfo();".PHP_EOL;
+	$code .= "?>";
+	
+	$fragment = new rex_fragment();
+	$fragment->setVar('class', 'info', false);
+	$fragment->setVar('title', $this->i18n('config_info'), false);
+	$fragment->setVar('body', rex_string::highlight($code), false);
+	echo $fragment->parse('core/page/section.php');
+	
+	///
+	
+	$code = "";
+	$code .= "<?php".PHP_EOL;
 	$code .= "	echo rex_address::getStreet();".PHP_EOL;
 	$code .= "?>";
 	
